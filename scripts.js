f83369e5b58defe1b979bc8ffa8bcef2bbd8173a
Marcel Oomens <mail@marceloomens.com>
2017-11-11 11:23:40 +0100

    Comprehensive updates
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- scripts.js
+++ scripts.js
@@ -1,43 +1,70 @@
 (function() {
-  // Upload buttons (security check and modal)
-  $(document).ready(function() {
-    $(".upload").click(function(evt) {
-      $("#security-modal").modal("show");
-    });
-  });
+  // Check Tor status
+  let secureserver = "https://secure.publeaks.nl"
+    , securereceipt = "https://secure.publeaks.nl/#/receipt"
+    , checktor = "https://secure.publeaks.nl/checktor"
+    , hiddenserver = "http://5karyquenden4d6k.onion"
+    , hiddenreceipt = "http://5karyquenden4d6k.onion/#/receipt";
+  var tor = false;
 
-  // Navbar location update
-  $(document).ready(function() {
-    let offset = $(".navbar").outerHeight();
-    // Cache selectors alongside destinations
-    // > map is used as filter here; I don't like it
-    var links = $(".navbar .nav-bordered .nav-link").map(function(){
-      var href = $(this).attr("data-destination");
-      if ($(href).length)
-        return { "link": this, "href": href };
-    });
-    // Bind to scroll event
-    $(window).scroll(function() {
-      var top = $(this).scrollTop();
-      // > map is used as filter/reduce here; I don't like it
-      var progress = links.map(function() {
-        // Remove the active class now; since we might as well
-        $(this["link"]).removeClass("active");
-        if ($(this["href"]).offset().top <= top+offset) return this["link"];
-      });
-      // Add the active class to the last matched selector
-      $(progress[progress.length-1]).addClass("active");
-    });
-  });
+  // I could rewrite this block with JQuery since its precense is guaranteed
+  try {
+    if (window.XMLHttpRequest) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          if (xmlhttp.getResponseHeader("x-check-tor") === "true") {
+            console.log("Detected a known Tor exit node");
+            tor = true;
+          } else {
+            console.log("No known Tor exit node detected");
+          }
+        }
+      }
+      xmlhttp.open("GET", checktor, true);
+      xmlhttp.send();
+    }
+  } catch(err) {
+    console.log("Error checking Tor status");
+    console.log(err);
+  }
 
-  // Smooth scrolling
   $(document).ready(function() {
+    // Register upload button click event
+    //   show security modal if not on Tor
+    //   otherwise just redirect...
+    $("button.upload").click(function(evt) {
+      if (tor) {
+        window.location.href = hiddenserver;
+      } else {
+        $("#security-modal a.btn-danger").attr("href", secureserver);
+        $("#security-modal").modal("show");
+      }
+    });
+
+    // Similarly register login button click event
+    $("button.login").click(function(evt) {
+      if (tor) {
+        window.location.href = hiddenreceipt;
+      } else {
+        $("#security-modal a.btn-danger").attr("href", securereceipt);
+        $("#security-modal").modal("show");
+      }
+    });
+
+    // Register video playback controls
+    $("#jumbotron button").click(function(evt) {
+      // console.log($("#jumbotron video")[0]);
+      $("#jumbotron video")[0].requestFullScreen();
+    });
+
     // Register smooth-scrolling events
+    //   can I normalize for scroll speed rather than time?
     $(".smooth-scrolling").click(function(evt) {
-      var target = $(evt.delegateTarget).attr("data-destination");
+      var target = $(evt.delegateTarget).attr("href");
       $("html, body").animate(
         { scrollTop: $(target).offset().top
-        }, 500);
+        }, 800);
     });
   });
 })();
