8119548d33cc7c8a065cbabb1d51f54e30f9bf60
Marcel Oomens <mail@marceloomens.com>
2017-12-31 15:54:27 +0800

    Kitchen sink
    Initial commit of the HTML5 kitchen sink
    
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

---
+++ mobile.js
@@ -0,0 +1,36 @@
+(function() {
+
+  // Check Tor status
+  let checktor = "https://secure.publeaks.nl/checktor"
+  var tor = false;
+
+  // I could rewrite this block with JQuery since its precense is guaranteed
+  try {
+    if (window.XMLHttpRequest) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          if (xmlhttp.getResponseHeader("x-check-tor") === "true") {
+            console.log("Detected a known Tor exit node");
+            tor = true;
+          } else {
+            console.log("No known Tor exit node detected");
+          }
+        }
+      }
+      xmlhttp.open("GET", checktor, true);
+      xmlhttp.send();
+    }
+  } catch(err) {
+    console.log("Error checking Tor status");
+    console.log(err);
+  }
+
+  $(document).ready(function() {
+    // Show security modal if appropriate
+    if (!tor) {
+      $("#security-modal").modal("show");
+    }
+  });
+
+})();
